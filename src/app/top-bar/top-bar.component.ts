bfcff005d36fafd058d5ac58268882afb176e1f3
Danny Tran <danny@J32P-394F-8974U-R521.local>
2019-08-18 13:06:20 -0400

    added basic components

---
+++ src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.scss']
+})
+export class TopBarComponent implements OnInit {
+  name = 'Store';
+  @Input() inputExample;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
