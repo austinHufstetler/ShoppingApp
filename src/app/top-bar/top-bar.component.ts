eb3531938da23f583bf24fd64d3f19127270f8ec
Austin Hufstetler <austinhufstetler@gmail.com>
2019-09-29 15:33:56 -0400

    styles and home icon

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart, faHome } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-top-bar',
@@ -9,6 +9,7 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 export class TopBarComponent implements OnInit {
   name = 'Store';
   cart = faShoppingCart;
+  home = faHome;
 
   @Input() inputExample;
   constructor() { }
